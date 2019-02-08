034984eec91a75806243b2d43635d4cfae4bd93f
achsahoo <achsahoo25@gmail.com>
2019-02-08 16:05:09 +0530

    updated commit

--- src/main/java/com/taxi/demo/OnlineTaxiApplication.java
+++ src/main/java/com/taxi/demo/OnlineTaxiApplication.java
@@ -8,6 +8,7 @@ public class OnlineTaxiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineTaxiApplication.class, args);
+		//online taxi booking application
 	}
 
 }
